678de8a2025a731924b23e54c55fee23f2aaafee
mayuresh-byte <mayureshpitambare@gmail.com>
2024-07-20 20:24:32 +0530

    Feat: RabbitMQ integration

--- admin/src/product/product.controller.ts
+++ admin/src/product/product.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Post, Put } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductDto } from './dto';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller('products')
 export class ProductController {
-    constructor(private productService: ProductService) {}
+    constructor(private productService: ProductService, @Inject('PRODUCT_SERVICE') private readonly client: ClientProxy) {}
 
     @Get()
     async all() {
+        this.client.emit('Hello', 'Hello From RabbitMQ !!');
         return this.productService.all();
     }
 
--- main/src/product/product.controller.ts
+++ main/src/product/product.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductDto } from './dto';
 import { Product } from './pdoruct.model';
 import { ProductService } from './product.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller('product')
 export class ProductController {
@@ -15,5 +16,10 @@ export class ProductController {
     @Get()
     async all() {
         return this.productService.all();
+    }
+
+    @EventPattern('Hello')
+    async greet(data: string) {
+        console.log(data);
     }
 }
